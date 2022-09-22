1a3531a07954e45eebbbbe9899095116aab5205c
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2022-09-22 10:10:09 +0700

    Initial commit

---
+++ app/src/main/java/com/mobcom117/assignment2/TabbedActivity.java
@@ -0,0 +1,45 @@
+package com.mobcom117.assignment2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mobcom117.assignment2.ui.main.SectionsPagerAdapter;
+import com.mobcom117.assignment2.databinding.ActivityTabbedBinding;
+
+public class TabbedActivity extends AppCompatActivity {
+
+    private ActivityTabbedBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityTabbedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TabbedActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mobcom117/assignment2/ui/home/HomeFragment.kt
@@ -0,0 +1,50 @@
+package com.mobcom117.assignment2.ui.home
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.mobcom117.assignment2.MainActivity
+import com.mobcom117.assignment2.R
+import com.mobcom117.assignment2.TabbedActivity
+import com.mobcom117.assignment2.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val tabbedActivityButton: Button = binding.goToTabbedActivityButton
+        tabbedActivityButton.setOnClickListener{
+            val intent: Intent = Intent(this.context, TabbedActivity::class.java)
+            startActivity(intent)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
